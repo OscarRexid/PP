a62d04e2ee6f5dab8c34eec51f1c4a7baa2c0a2f
OscarRexid <swezlayer@gmail.com>
2023-10-06 17:28:40 +0200

    Initial commit

---
+++ ProjectPipe/main.cpp
@@ -0,0 +1,59 @@
+#include "SFML/Graphics.hpp"
+#include "SFML/Window.hpp"
+#include "SFML/OpenGL.hpp"
+
+#include "imgui/imgui.h"
+#include "imgui/imgui-SFML.h"
+
+int main()
+{
+    // create the window
+    sf::RenderWindow window(sf::VideoMode(800, 600), "OpenGL", sf::Style::Default, sf::ContextSettings(32));
+    ImGui::SFML::Init(window);
+    //window.setVerticalSyncEnabled(true);
+
+    // activate the window
+    window.setActive(true);
+
+    // load resources, initialize the OpenGL states, ...
+
+    // run the main loop
+    sf::Clock deltaClock;
+    bool running = true;
+    while (running)
+    {
+        // handle events
+        
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+
+            ImGui::SFML::ProcessEvent(event);
+            if (event.type == sf::Event::Closed)
+            {
+                // end the program
+                running = false;
+            }
+            else if (event.type == sf::Event::Resized)
+            {
+                // adjust the viewport when the window is resized
+                glViewport(0, 0, event.size.width, event.size.height);
+            }
+        }
+        ImGui::SFML::Update(window, deltaClock.restart());
+
+        // clear the buffers
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        // draw...
+            
+        // end the current frame (internally swaps the front and back buffers)
+        ImGui::SFML::Render(window);
+        window.display();
+    }
+
+    // release resources...
+    ImGui::SFML::Shutdown();
+    return 0;
+  
+}
